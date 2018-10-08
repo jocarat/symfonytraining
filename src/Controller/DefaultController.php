70e49aa4dfcec8daca0f821204b845368a49b243
jeromeweill <jerome@ocarat.com>
2018-10-08 14:37:25 +0200

    install annotations et conf

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -11,10 +11,14 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 
 class DefaultController extends Controller
 {
 
+    /**
+     * @Route("/", name="homepage")
+     */
     public function index(): Response
     {
         return $this->render('default/index.html.twig');
