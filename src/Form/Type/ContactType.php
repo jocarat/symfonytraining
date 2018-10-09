5ef511f215039ad0cdf202c5729effe4db16fa94
jerome <jerome@ocarat.com>
2018-10-09 17:15:10 +0200

    base contact_form

--- src/Form/Type/ContactType.php
+++ src/Form/Type/ContactType.php
@@ -11,6 +11,7 @@ namespace App\Form\Type;
 
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -26,7 +27,7 @@ class ContactType extends AbstractType
             ->add('lastname', TextType::class)
             ->add('email', EmailType::class)
             ->add('subject', TextType::class)
-            ->add('message', TextType::class);
+            ->add('message', TextareaType::class);
     }
 
     public function configureOptions(OptionsResolver $resolver)
